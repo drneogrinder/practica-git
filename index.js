f55d3b1d3e61cd1cc9b3e8e378924b963cc0e9ba
drneogrinder <drneogrinder666@gmail.com>
2019-01-09 22:22:48 -0600

    Strings nuevos

--- index.js
+++ index.js
@@ -6,3 +6,9 @@ var name = 'Lord Farquad';
 //es6
 let age = 35;
 const birthday = '23/10/2000';
+
+console.log('Hello world!!!');
+
+//string interpolation
+console.log(`Mi cumpleaños es: ${birthday}`)
+console.log(`y 5 mas 10 es: ${5 + 10}`)
