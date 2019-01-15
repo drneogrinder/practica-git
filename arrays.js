8f2a5cd6c9a43c41a254eba63c6a0c6c1edf51b6
drneogrinder <drneogrinder666@gmail.com>
2019-01-14 21:35:44 -0600

    lufarto

--- arrays.js
+++ arrays.js
@@ -1,6 +1,12 @@
 const numbers = [1,2,3,4,5,6,7,8,9];
 const names =['shrek','fiona','farquad'];
 
+const values = [...names,...numbers ];
+console.log(values);
+
+const mutant = {name: 'Xavier', power: 'Telepatia', rank: 5}
+const mutant2 = {...mutant,name: 'Magneto', power: 'Magnetismo', rank: 4}
+
 //iterations
 for(let i=0;i<names.lenght;i++){
 	if(i==1){
@@ -33,3 +39,11 @@ const sum = (a,b) =>{return a + b};
 const resta = (a,b) => a - b;
 
 names.forEach(item => console.log(item));
+
+const fullnames=[];
+names.forEach(item => fullnames.push(item + 'Cendejas'));
+console.log(fullnames);
+
+const output2 = names.map(item => item+' Cendejas');
+console.log(output2);
+//names.map(item => )
